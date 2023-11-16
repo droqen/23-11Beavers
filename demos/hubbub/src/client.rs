545d3f13b8dd17e236bcb773db8af28191dbc4f9
Alexander Martin <hi@droqen.com>
2023-11-16 12:17:18 -0500

    hubbub for stress testing server->client messages

--- demos/hubbub/src/client.rs
+++ demos/hubbub/src/client.rs
@@ -1,120 +1,76 @@
+use ambient_api::core::input::messages;
 use ambient_api::core::transform::components::{rotation, translation};
 use ambient_api::prelude::ElementComponentExt;
 use ambient_api::{
-    core::rect::components::size_from_background_image, prelude::*, ui::ImageFromUrl,
+    core::messages::Frame, core::rect::components::size_from_background_image, prelude::*,
+    ui::ImageFromUrl,
 };
 
 use packages::pixie::components::*;
-use packages::this::assets;
+use packages::this::{assets, components::*, messages::TextToClient};
 // use packages::this::components::*;
 
 #[main]
 pub fn main() {
-    // let animtimeent = Entity::new().with(ui_anim_time(), 0.0).spawn();
-    // HubbubApp::el(animtimeent).spawn_interactive();
+    let cs_msg_counter = Entity::new()
+        .with(cs_msgs_rcvd(), 0)
+        .with(cs_msgs_rcvd_last_frame(), 0)
+        .spawn();
 
-    // let find_sents = query((sent_time(), sent_text())).build();
-    // ambient_api::core::messages::Frame::subscribe(move |_| {
-    //     if random::<f32>() < 0.01 {
-    //         Entity::new()
-    //             .with(sent_time(), game_time().as_secs_f32())
-    //             .with(sent_text(), "Hello World".into())
-    //             .spawn();
-    //     }
-    //     if random::<f32>() < 0.01 {
-    //         for (sent, (time, _text)) in find_sents.evaluate() {
-    //             if time + random::<f32>() * 4.0 > game_time().as_secs_f32() {
-    //                 entity::despawn(sent);
-    //             }
-    //         }
-    //     }
-    // });
-    Entity::new()
-        .with(pix_sprurl(), assets::url("loading_spinner_thick.png"))
-        .with(pix_place(), ivec2(100, 100))
-        .with(pix_size(), ivec2(40, 40))
-        .with(pix_z(), 0)
-        .spawn();
-    Entity::new()
-        .with(pix_string(), "Hello World".to_string())
-        .with(pix_place(), ivec2(100, 100))
-        .with(pix_z(), 0)
-        .spawn();
+    {
+        let cs_msg_counter = cs_msg_counter.clone();
+        TextToClient::subscribe(move |_ctx, msg| {
+            msg.text;
+            entity::mutate_component(cs_msg_counter, cs_msgs_rcvd(), |ct| *ct += 1);
+        });
+    }
+
+    {
+        Entity::new()
+            .with(pix_string(), "messages received each frame:".to_string())
+            .with(pix_place(), ivec2(100, 90))
+            .with(pix_z(), 0)
+            .spawn();
+        let ctr_strixie = Entity::new()
+            .with(pix_string(), "> 0".to_string())
+            .with(pix_place(), ivec2(100, 100))
+            .with(pix_z(), 0)
+            .spawn();
+
+        const MAX_CHARS_PER_LINE: usize = 100;
+        const MAX_CHARS_TOTAL: usize = 1000;
+
+        query((cs_msgs_rcvd(), cs_msgs_rcvd_last_frame())).each_frame(move |ctrs| {
+            for (ctr, (ct, ctlast)) in ctrs {
+                entity::mutate_component(ctr_strixie, pix_string(), |str| {
+                    let chars = str.len();
+                    *str = match chars > MAX_CHARS_TOTAL {
+                        // true => format!("{},\n{ct}", str[chars / 2..].to_string()), // over limit
+                        true => format!("> {ct}"), // over limit
+                        false => {
+                            let linebreaks = str.matches("\n").count();
+                            format!(
+                                "{},{}{}",
+                                str,
+                                match chars > (1 + linebreaks) * MAX_CHARS_PER_LINE {
+                                    true => "\n", // too many chars per line
+                                    false => " ",
+                                },
+                                ct
+                            )
+                        }
+                    };
+                });
+                entity::add_component(ctr, cs_msgs_rcvd_last_frame(), ct);
+                entity::add_component(ctr, cs_msgs_rcvd(), 0);
+            }
+        });
+    }
+
+    // Entity::new()
+    //     .with(pix_sprurl(), assets::url("loading_spinner_thick.png"))
+    //     .with(pix_place(), ivec2(100, 100))
+    //     .with(pix_size(), ivec2(40, 40))
+    //     .with(pix_z(), 5)
+    //     .spawn();
 }
-
-// #[element_component]
-// #[allow(non_snake_case)]
-// fn HubbubApp(hooks: &mut Hooks, _animtimeent: EntityId) -> Element {
-//     ambient_element::use_interval(hooks, 0.25, || {});
-//     let prog = match (game_time().as_secs_f32() / 0.25 % 3.00).floor() as u32 {
-//         0 => ".",
-//         1 => "..",
-//         2 => "...",
-//         _ => "?",
-//     }
-//     .to_string();
-//     let sents = ambient_element::use_query(hooks, (sent_time(), sent_text()));
-//     // for (sent, (time,text))in sents {}
-//     FlowColumn::el(
-//         sents
-//             .into_iter()
-//             .map(|(sent, (time, text))| Text::el(format!("{text} @ {time} {prog}"))),
-//     )
-//     .with_background(vec4(0., 0., 0., 0.9))
-//     .with_padding_even(10.)
-
-//     // Group::el([
-//     //     LayoutFreeCenter::el(
-//     //         ImageFromUrl {
-//     //             url: assets::url("loading_spinner_thick.png"),
-//     //         }
-//     //         .el()
-//     //         .with(width(), 15.)
-//     //         .with(height(), 15.),
-//     //         true,
-//     //         true,
-//     //     )
-//     //     .with(translation(), vec3(10., 10., 0.))
-//     //     .with(width(), 15.)
-//     //     .with(height(), 15.),
-//     //     LayoutFreeCenter::el(
-//     //         ImageFromUrl {
-//     //             url: assets::url("loading_spinner_thick.png"),
-//     //         }
-//     //         .el()
-//     //         .with(width(), 15.)
-//     //         .with(height(), 15.),
-//     //         true,
-//     //         true,
-//     //     )
-//     //     .with(translation(), vec3(10., 30., 0.))
-//     //     .with(rotation(), Quat::from_rotation_z(1.0))
-//     //     .with(width(), 15.)
-//     //     .with(height(), 15.),
-//     //     LayoutFreeCenter::el(
-//     //         ImageFromUrl {
-//     //             url: assets::url("loading_spinner_thick.png"),
-//     //         }
-//     //         .el()
-//     //         .with(width(), 15.)
-//     //         .with(height(), 15.),
-//     //         true,
-//     //         true,
-//     //     )
-//     //     .with(translation(), vec3(10., 50., 0.))
-//     //     .with(width(), 15.)
-//     //     .with(height(), 15.),
-//     // ])
-//     // // FlowColumn::el([FlowRow::el([
-//     // //     Text::el("Loading"),
-//     // //     ImageFromUrl {
-//     // //         url: assets::url("loading_spinner_thick.png"),
-//     // //     }
-//     // //     .el()
-//     // //     .with(width(), 15.)
-//     // //     .with(height(), 15.)
-//     // //     .with(rotation(), Quat::from_rotation_z(1.0)),
-//     // // ])])
-//     // // .with_background(vec4(0., 0., 0., 0.9))
-//     // // .with_padding_even(10.)
-// }
--- demos/hubbub/src/server.rs
+++ demos/hubbub/src/server.rs
@@ -1,4 +1,48 @@
-use ambient_api::prelude::*;
+use ambient_api::{core::app::components::name, core::messages::Frame, prelude::*};
+
+use packages::powerwidget::{components::*, concepts::Powerwidget};
+
+use packages::this::components::*;
+
+use packages::this::messages::TextToClient;
+// use packages::subhub::messages::{TextFromSubhub, TextInternalSubhub, TextToSubhub};
 
 #[main]
-pub fn main() {}
+pub fn main() {
+    init_scmsg_pw();
+}
+
+fn init_scmsg_pw() {
+    let pw_rel = Powerwidget {
+        pw_description: "garbage send_client_broadcast_reliable per frame".to_string(),
+        ..Powerwidget::suggested()
+    }
+    .make()
+    .with(name(), "scmsg-reliable".to_string())
+    .spawn();
+
+    let pw_un = Powerwidget {
+        pw_description: "garbage send_client_broadcast_unreliable per frame".to_string(),
+        ..Powerwidget::suggested()
+    }
+    .make()
+    .with(name(), "scmsg-unreliable".to_string())
+    .spawn();
+
+    Frame::subscribe(move |_| {
+        let messages_per_frame = entity::get_component(pw_rel, pw_calculated_value()).unwrap();
+        for _i in 0..messages_per_frame as u32 {
+            TextToClient {
+                text: "Reliable Message".to_string(),
+            }
+            .send_client_broadcast_reliable();
+        }
+        let messages_per_frame = entity::get_component(pw_un, pw_calculated_value()).unwrap();
+        for _i in 0..messages_per_frame as u32 {
+            TextToClient {
+                text: "Unreliable Message".to_string(),
+            }
+            .send_client_broadcast_unreliable();
+        }
+    });
+}
